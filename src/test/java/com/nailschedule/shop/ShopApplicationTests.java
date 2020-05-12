071c45b951fd59cc3119cd6238843abd1cf977b8
khoaowen <khoaowen>
2020-05-12 12:18:46 +0200

    Initial commit

---
+++ src/test/java/com/nailschedule/shop/ShopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nailschedule.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertThat(true).isTrue();
+    }
+
+}
